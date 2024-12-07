0446a11885bd5271722da48c5521e21f2b8f901e
Luiz Filipe Bartelega Penha <luizfpenha16@gmail.com>
2024-12-07 16:58:30 -0300

    add tags

--- sistema_gestao_tarefa/controle_tarefa/admin.py
+++ sistema_gestao_tarefa/controle_tarefa/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Projeto
 from .models import Tarefa
+from .models import Tag
 
 admin.site.register(Projeto)
 admin.site.register(Tarefa)
+admin.site.register(Tag)
--- sistema_gestao_tarefa/controle_tarefa/models.py
+++ sistema_gestao_tarefa/controle_tarefa/models.py
@@ -1,15 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Tag(models.Model):
+    nome = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.nome
+
 class Projeto(models.Model):
     nome = models.CharField(max_length=200)
     descricao = models.TextField(blank=True, null=True)
     criador = models.ForeignKey(User, on_delete=models.CASCADE, related_name='projetos_criados')
     membros = models.ManyToManyField(User, related_name='projetos', blank=True)
     data_criacao = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField(Tag, related_name='projetos', blank=True)
 
     def __str__(self):
         return f'{self.nome} - {self.criador}'
+
 
 class Tarefa(models.Model):
     STATUS = [
@@ -25,6 +33,7 @@ class Tarefa(models.Model):
     status = models.CharField(max_length=20, choices=STATUS, default='todo')
     data_criacao = models.DateTimeField(auto_now_add=True)
     data_prazo_final = models.DateField(null=True, blank=True)
+    tags = models.ManyToManyField(Tag, related_name='tarefas', blank=True)
 
     def __str__(self):
         return f'{self.titulo} - ({self.status})'
