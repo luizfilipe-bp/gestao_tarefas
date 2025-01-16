b71e71241736c557b5d1457b0bbb49060e5ccbcd
Luiz Filipe Bartelega Penha <luizfpenha16@gmail.com>
2025-01-16 14:43:14 -0300

    models list_display

--- sistema_gestao_tarefa/controle_tarefa/admin.py
+++ sistema_gestao_tarefa/controle_tarefa/admin.py
@@ -3,6 +3,15 @@ from .models import Projeto
 from .models import Tarefa
 from .models import Tag
 
-admin.site.register(Projeto)
-admin.site.register(Tarefa)
-admin.site.register(Tag)
+class ProjetoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'descricao', 'criador', 'data_criacao')
+
+class TarefaAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'descricao', 'status')
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
+
+admin.site.register(Projeto, ProjetoAdmin)
+admin.site.register(Tarefa, TarefaAdmin)
+admin.site.register(Tag, TagAdmin)
--- sistema_gestao_tarefa/controle_tarefa/models.py
+++ sistema_gestao_tarefa/controle_tarefa/models.py
@@ -4,9 +4,6 @@ from django.contrib.auth.models import User
 class Tag(models.Model):
     nome = models.CharField(max_length=50, unique=True)
 
-    def __str__(self):
-        return self.nome
-
 class Projeto(models.Model):
     nome = models.CharField(max_length=200)
     descricao = models.TextField(blank=True, null=True)
@@ -14,10 +11,6 @@ class Projeto(models.Model):
     membros = models.ManyToManyField(User, related_name='projetos', blank=True)
     data_criacao = models.DateTimeField(auto_now_add=True)
     tags = models.ManyToManyField(Tag, related_name='projetos', blank=True)
-
-    def __str__(self):
-        return f'{self.nome} - {self.criador}'
-
 
 class Tarefa(models.Model):
     STATUS = [
@@ -34,6 +27,3 @@ class Tarefa(models.Model):
     data_criacao = models.DateTimeField(auto_now_add=True)
     data_prazo_final = models.DateField(null=True, blank=True)
     tags = models.ManyToManyField(Tag, related_name='tarefas', blank=True)
-
-    def __str__(self):
-        return f'{self.titulo} - ({self.status})'
