41a98081c25caafb1fa8a5aa7df5175a6bebc912
Luiz Filipe Bartelega Penha <luizfpenha16@gmail.com>
2025-01-20 15:25:47 -0300

    excluir feito, adicionar necessario ajuste tags

--- sistema_gestao_tarefa/controle_tarefa/urls.py
+++ sistema_gestao_tarefa/controle_tarefa/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.projetos, name='projetos'),
     path('projetos/detalhes/<int:id>', views.projeto_detalhes, name='projeto_detalhes'),
-    path('projetos/adicionar', views.adicionar_projeto, name='adicionar_projeto'),
+    path('projetos/adicionar', views.projeto_adicionar, name='projeto_adicionar'),
+    path('projetos/excluir/<int:id>', views.projeto_excluir, name='projeto_excluir'),
 ]
--- sistema_gestao_tarefa/controle_tarefa/views.py
+++ sistema_gestao_tarefa/controle_tarefa/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.template import loader
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
@@ -19,10 +19,45 @@ def projetos(request):
     template = loader.get_template('projetos.html')
     return HttpResponse(template.render({'user': usuario_logado, 'projetos_do_usuario': projetos_do_usuario}))
 
+@login_required(login_url='/auth/login')
+def projeto_excluir(request, id):
+    projeto = Projeto.objects.get(id=id)
+    projeto.delete()
+    return redirect('projetos')
+
 
 @login_required(login_url='/auth/login')
-def adicionar_projeto(request):
-    pass
+def projeto_adicionar(request):
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        descricao = request.POST.get('descricao')
+        membros_ids = request.POST.getlist('membros')
+        tags_ids = request.POST.get('tags').split(',')
+        nova_tag_nome = request.POST.get('nova_tag')
+
+        # Criar projeto
+        projeto = Projeto.objects.create(
+            nome=nome,
+            descricao=descricao,
+            criador=request.user
+        )
+
+        membros = User.objects.filter(id__in=membros_ids)
+        projeto.membros.set(membros)
+
+        tags = Tag.objects.filter(id__in=[tid for tid in tags_ids if tid.isdigit()])
+        projeto.tags.set(tags)
+
+        if nova_tag_nome:
+            nova_tag, created = Tag.objects.get_or_create(nome=nova_tag_nome)
+            projeto.tags.add(nova_tag)
+
+        return redirect('projetos') 
+
+    usuarios = User.objects.exclude(id=request.user.id)
+    tags = Tag.objects.all()
+    return render(request, 'projeto_adicionar.html', {'usuarios': usuarios, 'tags': tags})
+
 
 
 @login_required(login_url='/auth/login')
